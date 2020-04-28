af06c85e8f0df35ae495ee693917ca9f94e32838
txshiro <adamturcann@gmail.com>
2020-04-28 18:05:27 +0200

    Update unmute.js

--- commands/admin/unmute.js
+++ commands/admin/unmute.js
@@ -5,25 +5,25 @@ module.exports.run = async (bot, message, args) => {
 
     if (!message.member.hasPermission(["MANAGE_ROLES", "MUTE_MEMBERS"])) return message.reply("You don't have enough permissions!");
 
-    let mutee = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    if (!mutee) return message.reply("You need to specify a user!")
-    if (mutee.id === message.author.id) return message.reply("You can't use this command on yourself.")
+    let unmutee = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    if (!unmutee) return message.reply("You need to specify a user!")
+    if (unmutee.id === message.author.id) return message.reply("You can't use this command on yourself.")
     if (unmutee.id === "694857173595062354") return message.reply("You can't use this command on bot.");
 
     let reason = args.slice(1).join(" ");
     if (!reason) reason = "No reason given."
 
-    let muterole = message.guild.roles.cache.find(r => r.name === "Muted");
-    if (mutee.roles.has(muterole.id)) return message.channel.send("That person isn't muted")
+    let unmuterole = message.guild.roles.cache.find(r => r.name === "Muted");
+    if (unmutee.roles.has(muterole.id)) return message.channel.send("That person isn't muted")
 
-    mutee.roles.remove(muterole.id).then(() => {
+    unmutee.roles.remove(unmuterole.id).then(() => {
         let embed = new Discord.MessageEmbed()
             .setTitle("You've been muted")
             .setAuthor(bot.user.username, bot.user.avatarURL())
             .setDescription(`**You've been unmuted in:** ${message.guild.name}\n**Reason**: ${reason}`)
             .setTimestamp()
             .setColor(color.green);
-        mutee.send(embed)
+        unmutee.send(embed)
         if (message.guild.id === "703661705997189200") {
             let mutechannel = message.guild.channels.cache.find(ch => ch.name === "mutes")
 
