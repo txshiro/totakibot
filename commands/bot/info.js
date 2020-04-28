433e3b9de3465583e29b79e1765e1f827cc9c002
txshiro <adamturcann@gmail.com>
2020-04-28 16:11:04 +0200

    Update info.js

--- commands/bot/info.js
+++ commands/bot/info.js
@@ -2,17 +2,16 @@ const Discord = require('discord.js');
 const package = require("../../package.json")
 const color = require("../../json/colors.json")
 const read = require('fs-readdir-recursive');
+const { readdirSync } = require('fs')
 
 module.exports.run = async (bot, message, args) => {
 
 
 
-    const files = read('../../commands');
-    files.forEach(file => {
-        let cmd = file.replace('.js', '.js');
-        let props = require(`./commands/${cmd}`);
+    readdirSync("./commands/").forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}/`).filter(f => f.endsWith(".js"));
+    })
 
-    });
 
     let embed = new Discord.MessageEmbed()
         .setAuthor(message.author.username, message.author.avatarURL())
@@ -21,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("📦 Node", `Version: ${process.version}`)
         .addField("📚 Library", `Discord.js: v${package.dependencies["discord.js"].substr(1)}`)
         .addField("💽 Memory", `${(parseInt(process.memoryUsage().heapUsed / 1024 / 1024))} / ${parseInt(process.memoryUsage().heapTotal / 1024 / 1024)}`)
-        .addField("Commands Count", file.length)
+        .addField("Commands Count", commands.length)
         .setFooter("tk!help for commands!", bot.user.avatarURL())
         .setColor(color.blueviolet)
         .setThumbnail(bot.user.avatarURL())
