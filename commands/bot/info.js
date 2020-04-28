366f126109ed3e6a6b1a5e7a5ec971b6ed64a716
txshiro <adamturcann@gmail.com>
2020-04-28 15:41:07 +0200

    Update info.js

--- commands/bot/info.js
+++ commands/bot/info.js
@@ -5,6 +5,15 @@ const read = require('fs-readdir-recursive');
 
 module.exports.run = async (bot, message, args) => {
 
+
+
+    const files = read('../../commands');
+    files.forEach(file => {
+        let cmd = file.replace('.js', '.js');
+        let props = require(`./commands/${cmd}`);
+
+    });
+
     let embed = new Discord.MessageEmbed()
         .setAuthor(message.author.username, message.author.avatarURL())
         .addField("👨 Created by", "txshiro#0612")
@@ -12,19 +21,14 @@ module.exports.run = async (bot, message, args) => {
         .addField("📦 Node", `Version: ${process.version}`)
         .addField("📚 Library", `Discord.js: v${package.dependencies["discord.js"].substr(1)}`)
         .addField("💽 Memory", `${(parseInt(process.memoryUsage().heapUsed / 1024 / 1024))} / ${parseInt(process.memoryUsage().heapTotal / 1024 / 1024)}`)
+        .addField("Commands Count", files.length)
         .setFooter("tk!help for commands!", bot.user.avatarURL())
         .setColor(color.blueviolet)
         .setThumbnail(bot.user.avatarURL())
         .setTimestamp();
 
-    const files = read('../../commands');
-    files.forEach(file => {
-        let cmd = file.replace('.js', '.js');
-        let props = require(`./commands/${cmd}`);
-        embed.addField("Commands Count", props.length)
-
-    });
     message.channel.send(embed)
+
 }
 
 module.exports.help = {
