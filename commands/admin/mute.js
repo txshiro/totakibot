469ff4665dfe23a90d85276a1e1e16cb7441953f
txshiro <adamturcann@gmail.com>
2020-04-29 10:55:37 +0200

    Update mute.js

--- commands/admin/mute.js
+++ commands/admin/mute.js
@@ -14,9 +14,7 @@ module.exports.run = async (bot, message, args) => {
 
     let time = args[1]
     if (!time) return message.reply("Please specify a time.")
-    if (time != Math.floor(time)) return message.reply("You can only use numbers")
-    if (time.startsWith(isDigitCode) && !time.endsWith("s") && !time.endsWith("h") && !time.endsWith("m") && !time.endsWith("d")) return message.reply("You can only use `s`, `m`, `h` or `d`");
-
+    if (time != Math.floor(time) && !time.endsWith("s") && !time.endsWith("h") && !time.endsWith("m") && !time.endsWith("d")) return message.reply("You can only use numbers")
 
     let reason = args.slice(2).join(" ");
     if (!reason) reason = "None"
