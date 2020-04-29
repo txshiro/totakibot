ea7d345f106638f4f0c9f3aa7b4af8ab44b17452
txshiro <adamturcann@gmail.com>
2020-04-29 08:13:21 +0200

    Update mute.js

--- commands/admin/mute.js
+++ commands/admin/mute.js
@@ -53,26 +53,27 @@ module.exports.run = async (bot, message, args) => {
             let mutechannel = message.guild.channels.cache.find(ch => ch.name === "mod-logs")
             if (!mutechannel) return message.reply(`Person was succesfully muted for ${time}. Please create a channel named 'mod-logs' If you want to send a log.`)
             setTimeout(function () {
-                mutee.roles.remove(muterole.id);
+                mutee.roles.remove(muterole.id).then(() => {
 
-                let embed4 = new Discord.MessageEmbed()
-                    .setTitle("You've been automatically unmuted")
-                    .setAuthor(bot.user.username, bot.user.avatarURL())
-                    .setThumbnail(mutee.user.avatarURL())
-                    .setDescription(`** You've been unmuted in:** ${message.guild.name}\n**Reason:** ${reason}\n**For:** ${time}\n**By:** ${message.author.tag}\n**Date:** ${message.createdAt.toLocaleString()}`)
-                    .setTimestamp()
-                    .setColor(color.red);
-                mutee.send(embed4)
+                    let embed4 = new Discord.MessageEmbed()
+                        .setTitle("You've been automatically unmuted")
+                        .setAuthor(bot.user.username, bot.user.avatarURL())
+                        .setThumbnail(mutee.user.avatarURL())
+                        .setDescription(`** You've been unmuted in:** ${message.guild.name}\n**Reason:** ${reason}\n**For:** ${time}\n**By:** ${message.author.tag}\n**Date:** ${message.createdAt.toLocaleString()}`)
+                        .setTimestamp()
+                        .setColor(color.red);
+                    mutee.send(embed4)
 
-                let mutechannel = message.guild.channels.cache.find(ch => ch.name === "mods-log")
-                let embed3 = new Discord.MessageEmbed()
-                    .setTitle(`${mutee.user.tag} was unmuted!`)
-                    .setThumbnail(mutee.user.avatarURL())
-                    .setAuthor(bot.user.username, bot.user.avatarURL())
-                    .setDescription(`** Muted by:** ${message.author.tag}\n ** Reason **: ${reason}\n ** Date:** ${message.createdAt.toLocaleString()}`)
-                    .setTimestamp()
-                    .setColor(color.red);
-                mutechannel.send(embed3);
+                    let mutechannel = message.guild.channels.cache.find(ch => ch.name === "mod-logs")
+                    let embed3 = new Discord.MessageEmbed()
+                        .setTitle(`${mutee.user.tag} was unmuted!`)
+                        .setThumbnail(mutee.user.avatarURL())
+                        .setAuthor(bot.user.username, bot.user.avatarURL())
+                        .setDescription(`** Muted by:** ${message.author.tag}\n ** Reason **: ${reason}\n ** Date:** ${message.createdAt.toLocaleString()}`)
+                        .setTimestamp()
+                        .setColor(color.red);
+                    mutechannel.send(embed3);
+                })
             }, ms(time));
         }
     })
