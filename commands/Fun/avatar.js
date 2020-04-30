cb68a1d428fcde5e8e661d16dbb63b9266dd6835
txshiro <adamturcann@gmail.com>
2020-04-30 14:18:30 +0200

    Create avatar.js

---
+++ commands/Fun/avatar.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+
+    if (!args[0]) {
+        var user = message.author;
+    } else {
+        var user = message.mentions.users.first() || bot.users.cache.get(args[0]);
+    }
+
+    if (user.id === message.author.id) {
+        let embed = new Discord.MessageEmbed()
+            .setAuthor(user, user.avatarURL())
+            .setTitle("Your avatar")
+            .setImage(user.avatarURL())
+            .setTimestamp()
+            .setFooter("tk!help for commands", bot.user.avatarURL())
+        return message.channel.send(embed)
+
+    } else {
+        let embed2 = new Discord.MessageEmbed()
+            .setAuthor(user, user.avatarURL())
+            .setTitle(`${user}'s avatar`)
+            .setImage(user.avatarURL())
+            .setTimestamp()
+            .setFooter("tk!help for commands", bot.user.avatarURL())
+        return message.channel.send(embed2)
+    }
+
+
+}
+
+module.exports.help = {
+    name: 'avatar',
+    aliases: [],
+    description: "Get user's avatar",
+    noaliases: "None",
+    accessability: "Everyone"
+}
