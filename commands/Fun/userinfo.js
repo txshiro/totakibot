c2f58897fb2abd8697fa8103f225731723a6809f
txshiro <adamturcann@gmail.com>
2020-05-02 19:00:42 +0200

    Update userinfo.js

--- commands/Fun/userinfo.js
+++ commands/Fun/userinfo.js
@@ -13,7 +13,6 @@ module.exports.run = async (bot, message, args) => {
 
     const joinedAt = formatDate(user.joinedAt)
     const createdAt = formatDate(user.createdAt)
-    const role = user.roles.filter(r => r.id !== message.guild.id).map(r => r).join(", ") || "none";
 
     let uEmbed = new Discord.MessageEmbed()
         .setColor('f5f242')
@@ -26,7 +25,6 @@ module.exports.run = async (bot, message, args) => {
         .addField("**Status:**", `${user.presence.status}`)
         .addField("**Created at:**", `${createdAt}`)
         .addField("**Joined at:**", joinedAt)
-        .addField("Roles", role)
         .setFooter(bot.user.username, bot.user.avatarURL())
         .setTimestamp()
 
